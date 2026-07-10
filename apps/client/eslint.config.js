ebb4abed8fc1acac037ef1b3754566bc88579163
Naomi Gilbert <naomi@morialkar.com>
2026-07-10 12:27:13 -0400

    chore: fix client eslint config

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -4,36 +4,30 @@ import tsParser from '@typescript-eslint/parser';
 import prettierConfig from 'eslint-config-prettier';
 
 export default [
+  {
+    ignores: ['dist', 'eslint.config.js'],
+  },
   js.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    ignores: ['vite.config.ts', 'vitest.config.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: ['./tsconfig.json', './tsconfig.app.json'],
+        project: ['./tsconfig.eslint.json'],
         tsconfigRootDir: import.meta.dirname,
-      },
-      globals: {
-        browser: true,
-        es2020: true,
-        window: 'readonly',
-        document: 'readonly',
-        fetch: 'readonly',
       },
     },
     plugins: {
       '@typescript-eslint': tseslint,
     },
     rules: {
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
   prettierConfig,
-  {
-    ignores: ['dist', 'eslint.config.js'],
-  },
 ];
